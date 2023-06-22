7575b1c19ab720eab8e73dd63d82346c9226e46b
Juan Estrella <jdavidstar23@gmail.com>
2023-06-22 17:30:17 -0500

    rutas update
    sa

--- JAS-SOFT/routes/web.php
+++ JAS-SOFT/routes/web.php
@@ -2,10 +2,15 @@
 
 
 
+
+
+
 use App\Models\Insumo;
 use App\Http\Controllers\BuzonsugerenciaController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+
+
 
 
 /*
@@ -19,61 +24,76 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
+
 Route::get('/', function () {
     return view("index");
 });
+
 
 Route::get('/rrhh', function () {
     return view("rrhh");
 });
 
+
 Route::get('/ventas', function () {
     return view("ventas");
 });
+
 
 Route::get('/sales', function () {
     return view("sales");
 });
 
+
 Route::get('/inventario', function () {
     return view("inventario");
 });
+
 
 Route::get('/login', function () {
     return view("login");
 });
 
+
 Route::get('/dashboard', function () {
     return view("dashboard");
 });
+
 
 Route::get('/sugerencia', function () {
     return view("sugerencia");
 });
 
+
 Route::get('/agregar', function () {
     return view("agregar");
 });
+
 
 Route::get('/buzon', function () {
     return view("buzon");
 });
 
-Route::get('/postulaciones', function () {
-    return view("postulaciones");
+
+Route::get('/postulacion', function () {
+    return view("postulacion");
 });
+
 
 Route::get('/register', function () {
     return view("register");
 });
 
+
 Auth::routes();
 Route::get('insumos/pdf', [InsumoController::class, 'pdf'])->name('insumo.pdf');
-Route::get('venta/pdf', [VentumController::class, 'pdf'])->name('venta.pdf');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
+
+
 use App\Http\Controllers\RegisterController;
+
 
 Route::get('/registro', [RegisterController::class, 'create'])->name('registro.formulario');
 Route::post('/registro', [RegisterController::class, 'store'])->name('registro.almacenar');
@@ -82,7 +102,10 @@ Route::get('/registro/exitoso', function () {
 })->name('registro.exitoso');
 
 
+
+
 use App\Http\Controllers\InsumoController;
+
 
 Route::post('/registerInsumo', [InsumoController::class, 'store'])->name('registerInsumo.almacenar');
 Route::get('/insumos', [InsumoController::class, 'index'])->name('insumo.listar');
@@ -91,7 +114,10 @@ Route::post('insumos/eliminar', [InsumoController::class, 'destroy'])->name('ins
 Route::post('insumos/actualizar', [InsumoController::class, 'update'])->name('insumo.actualizarinsumo');
 
 
+
+
 use App\Http\Controllers\OrdenpedidoController;
+
 
 Route::post('/registerordenpedido', [OrdenpedidoController::class, 'store'])->name('ordenpedido.almacenar');
 Route::get('/ordenpedidos', [OrdenpedidoController::class, 'index'])->name('ordenpedido.listar');
@@ -99,13 +125,16 @@ Route::post('/ordenpedidos/editar', [OrdenpedidoController::class, 'edit'])->nam
 Route::post('ordenpedidos/eliminar', [OrdenpedidoController::class, 'destroy'])->name('ordenpedido.eliminar');
 Route::post('ordenpedidos/actualizar', [OrdenpedidoController::class, 'update'])->name('ordenpedido.actualizarordenpedido');
 
+
 use App\Http\Controllers\ProductoController;
+
 
 Route::post('/registerproducto', [ProductoController::class, 'store'])->name('producto.almacenar');
 Route::get('/productos', [ProductoController::class, 'index'])->name('producto.listar');
 Route::post('/productos/editar', [ProductoController::class, 'edit'])->name('producto.edit');
 Route::post('productos/eliminar', [ProductoController::class, 'destroy'])->name('producto.eliminar');
 Route::post('productos/actualizar', [ProductoController::class, 'update'])->name('producto.actualizarproducto');
+
 
 use App\Http\Controllers\VentumController;
 
@@ -117,7 +146,10 @@ Route::post('/ventum/eliminar', [VentumController::class, 'destroy'])->name('ven
 Route::post('/ventum/actualizar', [VentumController::class, 'update'])->name('ventum.actualizarordenpedido');
 
 
+
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 
 Route::post('/registerbuzonsugerencia', [BuzonsugerenciaController::class, 'store'])->name('buzonsugerencia.almacenar');
 Route::get('/buzonsugerencias', [BuzonsugerenciaController::class, 'index'])->name('buzonsugerencia.listar');
@@ -126,8 +158,11 @@ Route::post('buzonsugerencias/eliminar', [BuzonsugerenciaController::class, 'des
 Route::post('buzonsugerencias/actualizar', [BuzonsugerenciaController::class, 'update'])->name('buzonsugerencia.actualizarBuzonsugerencia');
 
 
-Route::get('/ventum', [VentumController::class, 'index'])->name('ventum.index');
+use App\Http\Controllers\PostulacionController;
 
-Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::post('/registerpostulacion', [PostulacionController::class, 'store'])->name('postulacion.almacenar');
+Route::get('/postulacion', [PostulacionController::class, 'index'])->name('postulacion.listar');
+Route::post('/postulacion/editar', [PostulacionController::class, 'edit'])->name('postulacion.edit');
+Route::post('/postulacion/eliminar', [PostulacionController::class, 'destroy'])->name('postulacion.eliminar');
+Route::post('/postulacion/actualizar', [PostulacionController::class, 'update'])->name('postulacion.actualizarpostulacion');
