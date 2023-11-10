8292c74395b882ad6676580882e40768f1e55d4e
Sanfor20 <santiteus2005@gmail.com>
2023-11-10 01:09:44 -0500

    App Suggestions

---
+++ jas_softpy/suggestions/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Suggestions(models.Model):
+    
+    category = models.CharField(max_length=50, verbose_name="Categoria")
+
+
+    def  _str_(self):
+        return self.category
+    
+    class Meta:
+        verbose_name = "Sugerencias"
+        verbose_name_plural = "Sugerencias"
+        db_table = "buzonsugerencias"
+        ordering = ['id']
