364cf017e862dd1478cadfccaa85e3bb7636aebd
Cristian Devia <124652905+cristian-devia@users.noreply.github.com>
2023-11-10 01:49:55 -0500

    Revert "admin suggestions"
    This reverts commit cffbde33aa3d7f3a27066aee234b9e552982f83f.

--- jas_softpy/suggestions/admin.py
+++ jas_softpy/suggestions/admin.py
@@ -1,18 +1,3 @@
 from django.contrib import admin
-from .models import Suggestions
 
 # Register your models here.
-
-
-
-
-
-
-@admin.register(Suggestions)
-class SuggestionsAdmin(admin.ModelAdmin):
-    list_display = ('category', 'descriptCategory' )
-    search_fields = ('category')
-    list_editable = ()
-    list_filter = ('category')
-    
-
