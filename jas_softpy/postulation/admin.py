66a7cc875ef6b91f856dba6dc322e8950a10b922
jdstar23 <127244478+jdstar23@users.noreply.github.com>
2023-11-15 14:19:14 -0500

    Admin Postulation

--- jas_softpy/postulation/admin.py
+++ jas_softpy/postulation/admin.py
@@ -1,3 +1,10 @@
+from turtle import position
 from django.contrib import admin
+from .models import Postulation,State_Postulations,Employed,Contract
 
-# Register your models here.
+@admin.register(Postulation)
+class PostulationAdmin(admin.ModelAdmin):
+    list_display = ('start_offers', 'descrip_offer','profile_postulation','Employed','Details_Offer','State_Postulations')
+    search_fields = ('Employed',)
+    list_editable = ()
+    list_filter = ('SaleDate',)
