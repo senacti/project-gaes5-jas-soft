b5e91ca045764f9add5c2058378e98f7e182aa85
MariaFAlvarado <126290517+mariafalavarado@users.noreply.github.com>
2023-11-21 22:39:57 -0500

    Import-Export Product

--- jas_softpy/inventory/admin.py
+++ jas_softpy/inventory/admin.py
@@ -1,21 +1,32 @@
 from django.contrib import admin
 from .models import Product
 from .models import Flow
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 #admin.site.register(Supplies)
 #admin.site.register(ProductionOrder)
 
 @admin.register(Product)
-class SupplieAdmin(admin.ModelAdmin):
+class ProductAdmin(ImportExportModelAdmin):
     list_display = ('name', 'stock','fabricationDate','size','color','state',)
     search_fields = ('name',)
     list_editable = ('stock',)
     list_filter = ('fabricationDate',)
     
 @admin.register(Flow)
-class FlowAdmin(admin.ModelAdmin):
+class FlowAdmin(ImportExportModelAdmin):
     list_display = ('FlowType','DateFlow','Product','supplies')    
     search_fields = ('FlowType',)
     list_filter = ('DateFlow',)
 
 # Register your models here.
+
+class  ProductResource(resources.ModelResource):
+    class Meta:
+        model = Product
+        fields = ('name', 'stock','fabricationDate','size','color','state',)
+        #export_order = ('name
+        # ', 'price', 'category')    
+        
+    
