6fa759afd665864c09c9b9660931dc6fe7de90c1
jdstar23 <127244478+jdstar23@users.noreply.github.com>
2023-11-24 14:34:45 -0500

    Excel Postulation

--- jas_softpy/postulation/admin.py
+++ jas_softpy/postulation/admin.py
@@ -1,22 +1,30 @@
 from django.contrib import admin
 from .models import Postulation,Employed,Contract
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 @admin.register(Postulation)
-class PostulationAdmin(admin.ModelAdmin):
+class PostulationAdmin(ImportExportModelAdmin):
     list_display = ('startOffers', 'descripOffer','profilePostulation','StatePostulations',)
     search_fields = ('descripOffer',)
     list_editable = ()
     list_filter = ('startOffers',)
 
+class SupplieResource(resources.ModelResource):
+    class Meta:
+        model = Postulation
+        fields = ('startOffers', 'descripOffer','StatePostulations','profilePostulation')
+        #export_order = ('startOffers', 'descripOffer', 'profilePostulation')
+
 @admin.register(Contract)
-class ContractAdmin(admin.ModelAdmin):
+class ContractAdmin(ImportExportModelAdmin):
     list_display = ('contractdate', 'TypeContract',)
     search_fields = ('TypeContract',)
     list_editable = ()  
     list_filter = ('TypeContract',) 
-
+    
 @admin.register(Employed)
-class EmployedAdmin(admin.ModelAdmin):
+class EmployedAdmin(ImportExportModelAdmin):
     list_display = ('name', 'position',)
     #list_display_links = ('name')
 
