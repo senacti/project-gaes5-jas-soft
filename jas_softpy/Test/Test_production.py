4a960b1a73f6db067594e68a7f9bc2c2b4484baf
Alejandro Ospina <oa.ospinabenitez@gmail.com>
2024-03-13 10:07:44 -0500

    test

--- jas_softpy/Test/Test_production.py
+++ jas_softpy/Test/Test_production.py
@@ -1,14 +1,26 @@
 import sys
-sys.path.append('/jas_softpy/jas_softpy/production')
+
+import pytest
+sys.path.append('jas_softpy')
 
 import os
-from django.test import TestCase
-import django
-import pytest
-from jas_softpy.production.models import Supplies
 
-#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jas_softpy.settings')
-django.setup()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jas_softpy.settings')
+
+from django.test import TestCase
+from django.core.exceptions import ImproperlyConfigured
+from django.conf import settings
+
+from production.models import Supplies
+
+# We need to do this here, before any of our tests run, since they
+# might try to use the INSTALLED_APPS setting.
+try:
+    settings.INSTALLED_APPS
+except ImproperlyConfigured as e:
+    if "The SECRET_KEY setting must not be empty." in str(e):
+        pytest.skip("Can't run tests without a SECRET_KEY in the environment.")
+    raise
 
 class TestSupplies(TestCase):
     
@@ -28,4 +40,6 @@ class TestSupplies(TestCase):
         self.assertEqual(supplie.stock, '49')
         self.assertEqual(supplie.size, 'L')
         self.assertEqual(supplie.color, 'Negro')
-        self.assertEqual(supplie.supplieCode, '100001')
+        self.assertEqual(supplie.suppliesCode, '100001')
+        self.assertEqual(supplie.suppliesCode, '100001')
+
