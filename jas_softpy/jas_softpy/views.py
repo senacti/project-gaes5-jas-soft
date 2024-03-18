b4c184a32517eceab26821347cee2846f5379d8d
Sanfor20 <126290208+Sanfor20@users.noreply.github.com>
2024-03-17 22:29:36 -0500

    Revert "Update views.py"
    This reverts commit 9067aa723f4de8451a7473fc996c7057479fa4da.

--- jas_softpy/jas_softpy/views.py
+++ jas_softpy/jas_softpy/views.py
@@ -95,32 +95,27 @@ def sales(request):
         #context
     })   
 
-def login_view(request):
+def login_view(request):    
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
 
         user = authenticate(username=username, password=password)
         if user:
-            if user.is_staff:
-                login(request, user)
-                messages.success(request, 'Bienvenido {}'.format(user.username))
-                return redirect('admin:index')
-            else:
-                login(request, user)
-                return home(request)
-
+            login(request, user)
+            messages.success(request, 'Bienvenido {}'.format(user.username))
+            return redirect('admin:index')
         else:
             messages.error(request, 'Usuario o contraseña incorrectos')
 
     return render(request, 'login.html',{
-
-})
+        
+    })
 
 def logout_view(request):
     logout(request)
     messages.success(request, 'Sesión finalizada')
-    return redirect('login_jas')
+    return redirect('index')
 
 def register(request):    
 
